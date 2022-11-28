a1f3caddd325e4fe57cefce585be07b6fad7ac80
igor-aguiar <desenvigor@gmail.com>
2022-11-28 20:13:46 -0300

    Using divercified data to test a method

--- Alura.Estacionamento.VehicleTests/ParkingLotTest.cs
+++ Alura.Estacionamento.VehicleTests/ParkingLotTest.cs
@@ -1,4 +1,5 @@
-﻿using Alura.Estacionamento.Modelos;
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
 using System;
 using Xunit;
 
@@ -7,7 +8,7 @@ namespace Alura.Estacionamento.Tests
     public class ParkingLotTest
     {
         [Fact]
-        public void ParkingLotIncoming()
+        public void ValidateParkingLotIncoming()
         {
             var parkingLot = new Patio();
             
@@ -23,6 +24,33 @@ namespace Alura.Estacionamento.Tests
 
             Assert.Equal(2, totalIncoming);
 
+        }
+        [Theory]
+        [InlineData("Igor Aguiar", "abc-1234", "verde", TipoVeiculo.Motocicleta)]
+        [InlineData("Rosa Linda", "wav-5576", "vermelho", TipoVeiculo.Motocicleta)]
+        [InlineData("William Ducker", "asf-2243", "rosa", TipoVeiculo.Automovel)]
+        [InlineData("Pedro Marins", "pov-4321", "amarelo", TipoVeiculo.Automovel)]
+        public void ValidateParkingLotIncomingManyVehicles(string name, string plate, string color, TipoVeiculo tipoVeiculo)
+        {
+            var parkingLot = new Patio();
+            var vehicle = new Veiculo();
+            vehicle.Placa = plate;
+            vehicle.Proprietario = name;
+            vehicle.Cor = color;
+            vehicle.Tipo = tipoVeiculo;
+
+            parkingLot.RegistrarEntradaVeiculo(vehicle);
+            parkingLot.RegistrarSaidaVeiculo(vehicle.Placa);
+
+            double totalIncoming = parkingLot.TotalFaturado();
+            if (vehicle.Tipo == TipoVeiculo.Motocicleta)
+            {
+                Assert.Equal(1, totalIncoming);
+            } else
+            {
+                Assert.Equal(2, totalIncoming);
+            }
+            
         }
     }
 }
